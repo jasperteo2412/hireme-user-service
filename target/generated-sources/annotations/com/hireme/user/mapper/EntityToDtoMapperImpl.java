61aff0a80bca6424e7c42dd96770a5289907013d
Jasper Teo <43600109+jasperteo2412@users.noreply.github.com>
2023-10-20 12:42:32 +0800

    Updated config files for aws

--- target/generated-sources/annotations/com/hireme/user/mapper/EntityToDtoMapperImpl.java
+++ target/generated-sources/annotations/com/hireme/user/mapper/EntityToDtoMapperImpl.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2023-10-20T00:37:13+0800",
+    date = "2023-10-20T12:33:56+0800",
     comments = "version: 1.4.1.Final, compiler: javac, environment: Java 17.0.8 (Oracle Corporation)"
 )
 @Component
