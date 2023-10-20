11f8b48ce286c8b38498f8cc6a649f088075223e
Jasper Teo <43600109+jasperteo2412@users.noreply.github.com>
2023-10-20 13:59:30 +0800

    Updated buildpsec config

--- target/generated-sources/annotations/com/hireme/user/mapper/EntityToDtoMapperImpl.java
+++ target/generated-sources/annotations/com/hireme/user/mapper/EntityToDtoMapperImpl.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2023-10-20T13:42:19+0800",
+    date = "2023-10-20T13:59:00+0800",
     comments = "version: 1.4.1.Final, compiler: javac, environment: Java 17.0.8 (Oracle Corporation)"
 )
 @Component
