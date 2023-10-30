31fca9e5fc1d7e292f3d1b4ffa8239c17578c37f
Jasper Teo <43600109+jasperteo2412@users.noreply.github.com>
2023-10-30 12:12:58 +0800

    Added junit testing for user service

--- src/test/java/com/hireme/user/DemoApplicationTests.java
+++ src/test/java/com/hireme/user/DemoApplicationTests.java
@@ -1,13 +1,10 @@
 package com.hireme.user;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class DemoApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
